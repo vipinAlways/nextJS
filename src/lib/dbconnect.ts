e48ffb57fcda8ed3e71abec7c3d120c32d351bf1
Your New Name <vipinkumarti555@gmail.com>
2024-05-25 23:21:33 +0530

    added data base

---
+++ src/lib/dbconnect.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+type ConnectionObject={
+    isConnected ?:number
+}
+const connection :ConnectionObject ={}
+
+export default async function dbConnect():Promise<void> {
+    if (connection.isConnected) {
+        console.log("Already connected to db");
+        return
+    }
+    try {
+        const db=await mongoose.connect(process.env.MOGODB_URI || '',{})
+        connection.isConnected=db.connections[0].readyState
+        console.log("db connected successfully");
+    } catch (error) {
+        console.error("db connection failed",error);
+        process.exit(1)
+    }
+}
+
